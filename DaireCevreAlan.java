b3013f6bcb57ffb3e634b971ace7d50202684592
goekaay <105244598+goekaay@users.noreply.github.com>
2022-11-17 21:07:29 +0300

    Add files via upload

---
+++ DaireCevreAlan.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class DaireCevreAlan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		int yariCap, aci;
+		double pi = 3.14, alan, cevre, sonuc;
+		
+		System.out.println("Dairenin yaricapini giriniz: ");
+		yariCap = s.nextInt();
+		System.out.println("Merkez açýsýnýn derecesini giriniz:");
+		aci = s.nextInt();
+		
+		alan = pi * yariCap * yariCap;
+		cevre = 2 * pi * yariCap;
+		sonuc = (pi*(yariCap * yariCap) * aci)/360;
+		
+		
+		System.out.println("Dairenin alaný="+ alan +"\nDairenin çevresi="+cevre);
+		System.out.println("Daire diliminin alaný= "+sonuc);
+		
+
+	}
+
+}
